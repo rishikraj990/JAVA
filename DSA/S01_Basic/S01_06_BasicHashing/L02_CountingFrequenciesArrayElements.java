30cadc59514f349faccaa99de1d51826f50c4a05
rishik <rishikraj990@gmail.com>
2026-06-19 01:47:45 +0530

    Added Basic Hashing

--- DSA/S01_Basic/S01_06_BasicHashing/L02_CountingFrequenciesArrayElements.java
+++ DSA/S01_Basic/S01_06_BasicHashing/L02_CountingFrequenciesArrayElements.java
@@ -1,4 +1,4 @@
-package DSA.S01_06_BasicHashing;
+package DSA.S01_Basic.S01_06_BasicHashing;
 
 import java.util.*;
 
@@ -7,23 +7,21 @@ public class L02_CountingFrequenciesArrayElements {
     /**
      * Problem Statement: Given an array, we have found the number of occurrences of each element in the array.
      *
-     * Examples
      * Example 1:
      * Input: arr[] = {10,5,10,15,10,5};
      * Output: 10  3
-     * 	            5  2
-     *                 15  1
+     * 	       5   2
+     *         15  1
      * Explanation: 10 occurs 3 times in the array
-     * 	      5 occurs 2 times in the array
-     *               15 occurs 1 time in the array
-     *
+     * 	            5 occurs 2 times in the array
+     *              15 occurs 1 time in the array
      * Example2:
      * Input: arr[] = {2,2,3,4,4,2};
      * Output: 2  3
-     * 	           3  1
-     *                4  2
+     * 	       3  1
+     *         4  2
      * Explanation: 2 occurs 3 times in the array
-     * 	     3 occurs 1 time in the array
+     * 	            3 occurs 1 time in the array
      *              4 occurs 2 time in the array
      */
 
@@ -45,7 +43,9 @@ public class L02_CountingFrequenciesArrayElements {
     }
 
     public static List<List<Integer>> countFrequencies(int[] nums) {
-
+//      /**
+//       * Optimal; TC:[ O(N) ]; SC:[ O(N) ]
+//       ====================================
         HashMap<Integer, Integer> store = new HashMap<>();
         for (int num : nums) {
             if (store.containsKey(num)) store.put(num, store.get(num) + 1);
@@ -60,5 +60,7 @@ public class L02_CountingFrequenciesArrayElements {
             result.add(tempResult);
         }
         return result;
+//       ====================================
+//       */
     }
 }
--- DSA/S01_Basic/S01_06_BasicHashing/L03_FindHigt_lowtFreqElm.java
+++ DSA/S01_Basic/S01_06_BasicHashing/L03_FindHigt_lowtFreqElm.java
@@ -1,4 +1,4 @@
-package DSA.S01_06_BasicHashing;
+package DSA.S01_Basic.S01_06_BasicHashing;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,12 +8,10 @@ public class L03_FindHigt_lowtFreqElm {
     /**
      * Problem Statement: Problem Statement: Given an array of size N. Find the highest and lowest frequency element.
      *
-     * Examples
      * Example 1:
      * Input: array[] = {10,5,10,15,10,5};
      * Output: 10 15
      * Explanation: The frequency of 10 is 3, i.e. the highest and the frequency of 15 is 1 i.e. the lowest.
-     *
      *
      * Example 2:
      * Input: array[] = {2,2,3,4,4,2};
@@ -29,25 +27,35 @@ public class L03_FindHigt_lowtFreqElm {
         for (int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.println(mostFrequentElement(arr));
-        System.out.println(leastFrequentElement(arr));
+        HashMap<Integer, Integer> store = preStoreHash(arr);
+        System.out.println(mostFrequentElement(store));
+        System.out.println(leastFrequentElement(store));
     }
-    public static int mostFrequentElement(int[] nums) {
-        HashMap<Integer, Integer> store = preStoreHash(nums);
+//      /**
+//       * Optimal; TC:[ O(3N) ]; SC:[ O(N) ]
+//       ====================================
+    public static int mostFrequentElement(HashMap<Integer, Integer> store) {
         int max = Integer.MIN_VALUE;
+        int res = Integer.MIN_VALUE;
         for(Map.Entry<Integer, Integer> keyValue : store.entrySet()){
-            max = max < keyValue.getKey() ? keyValue.getKey() : max;
+            if(max < keyValue.getValue()){
+                res = keyValue.getKey();
+                max = keyValue.getValue();
+            }
         }
-        return max;
+        return res;
     }
 
-    public static int leastFrequentElement(int[] nums) {
-        HashMap<Integer, Integer> store = preStoreHash(nums);
+    public static int leastFrequentElement(HashMap<Integer, Integer> store) {
         int min = Integer.MAX_VALUE;
+        int res = Integer.MAX_VALUE;
         for(Map.Entry<Integer, Integer> keyValue : store.entrySet()){
-            min = min > keyValue.getKey() ? keyValue.getKey() : min;
+            if(min > keyValue.getValue()){
+                res = keyValue.getKey();
+                min = keyValue.getValue();
+            }
         }
-        return min;
+        return res;
     }
 
     private static HashMap<Integer, Integer> preStoreHash(int[] nums) {
@@ -58,5 +66,6 @@ public class L03_FindHigt_lowtFreqElm {
         }
         return store;
     }
-
+//       ====================================
+//       */
 }
